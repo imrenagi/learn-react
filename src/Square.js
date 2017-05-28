12f5ba1670ccbdc8f53d83a54ea8212cbac6ae25
Imre Nagi <imre.nagi2812@gmail.com>
2017-05-27 17:34:00 -0700

    Init commit

---
+++ src/Square.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './index.css';
+
+export default class Square extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value : null,
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
+  }
+};
+
+
+
+// export default Game;
